66b3381f08c09910cf71a4956b77f5428342b105
Pronab pal <pronab@keybytesystems.com>
2025-03-27 00:14:13 +1100

    mini

--- LLM_Handson/python_langchain/langchain-chat/minRag.py
+++ LLM_Handson/python_langchain/langchain-chat/minRag.py
@@ -27,8 +27,6 @@ docs = [Document(page_content=raw_text)]
 
 
 
-# docs = loader.load()
-
 # 2. Split into semantically coherent chunks
 splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 chunks = splitter.split_documents(docs)
